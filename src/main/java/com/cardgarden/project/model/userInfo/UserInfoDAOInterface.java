24fdd0b2ba4bb9f4a18e4eb8aa884d7924c2b4a6
ljhpaul <colosair@gmail.com>
2025-06-17 17:40:39 +0900

    feat: UserInfo model 추가
    UserInfo model 추가

---
+++ src/main/java/com/cardgarden/project/model/userInfo/UserInfoDAOInterface.java
@@ -0,0 +1,13 @@
+package com.cardgarden.project.model.userInfo;
+
+import java.util.List;
+
+public interface UserInfoDAOInterface {
+	
+	public List<UserInfoDTO> selectAll();
+	public UserInfoDTO selectById(int id);
+	public int insert(UserInfoDTO dto);
+	public int update(UserInfoDTO dto);
+	public int delete(int id);
+	
+}
