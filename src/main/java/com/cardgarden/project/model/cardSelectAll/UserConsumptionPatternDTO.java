de1acd047e6abcfe4fd4d33b3d5750bd560c51b4
Ch27s <dbcotmd0@gmail.com>
2025-06-17 16:27:35 +0900

    채승 : 카드조회,소비패턴 입력
    카드 조회 화면 + controller,Model 생성
    소비패턴 입력 화면 구성

---
+++ src/main/java/com/cardgarden/project/model/cardSelectAll/UserConsumptionPatternDTO.java
@@ -0,0 +1,17 @@
+package com.cardgarden.project.model.cardSelectAll;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class UserConsumptionPatternDTO {
+	
+	private int benefitcategory_id;
+	private String benefitCategory_name;
+
+}
