98e40ef01b326a651d2486222244f52fa548c062
Ch27s <dbcotmd0@gmail.com>
2025-06-17 16:57:55 +0900

    카드 전체조회 화면 구현, controller, Model 구현

--- src/main/java/com/cardgarden/project/controller/CardController.java
+++ src/main/java/com/cardgarden/project/controller/CardController.java
@@ -1,8 +1,41 @@
 package com.cardgarden.project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cardgarden.project.model.selectCard.UserConsumptionPatternDTO;
+import com.cardgarden.project.model.selectCard.UserConsumptionPatternService;
+@Controller
 public class CardController {
 	
-	//깃 테스트
+	@Autowired
+	UserConsumptionPatternService ucpService;
+	
+	String namespace = "com.cardgarden.inCon.";
+	
+	@GetMapping("/inCon.do")
+	public String insertView(Model model) {
+		
+	
+		List<UserConsumptionPatternDTO>  benefitCategorylist = ucpService.selectAll();
+		
+		System.out.println(benefitCategorylist.size());
+	    model.addAttribute("benefitCategorylist", benefitCategorylist); // JSP에서 이 이름으로 사용 가능
+		
+		return "cardgarden/insertUserConsumptionPattern"; // 뷰 이름
+		
+	}
+	
+	@GetMapping("/cardAll.do")
+	public String cardall() {
+		
+		return "cardgarden/cardAll";
+	}
+
 	
 
 }
