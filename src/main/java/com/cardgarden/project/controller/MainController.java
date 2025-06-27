271aa8fe1fb642101722b6c6f08de3e983f5513a
ljhpaul <colosair@gmail.com>
2025-06-27 17:08:23 +0900

    feat: 회원가입 제약조건 추가
    feat: 회원가입 제약조건 추가

--- src/main/java/com/cardgarden/project/controller/MainController.java
+++ src/main/java/com/cardgarden/project/controller/MainController.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main")
+@RequestMapping("")
 public class MainController {
 	
-	//메인화면 연결
+	//메인으로 리다이렉트
 	@GetMapping("")
+	public String redirectToMain() {
+		return "redirect:/main";
+	}
+	
+	//메인화면 연결
+	@GetMapping("/main")
 	public String mainView() {
 		return "main";
 	}
