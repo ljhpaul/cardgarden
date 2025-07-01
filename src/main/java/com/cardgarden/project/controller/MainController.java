447e330b176165bb6c9dc9e343e9f22a50feb8eb
Gaegulin <121297188+Gaegulin@users.noreply.github.com>
2025-07-01 14:19:33 +0900

    fix: 메인 컨트롤러 redirect 수정

--- src/main/java/com/cardgarden/project/controller/MainController.java
+++ src/main/java/com/cardgarden/project/controller/MainController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("")
@@ -14,7 +15,7 @@ public class MainController {
 	
 	//메인으로 리다이렉트
 	@GetMapping("")
-	public String redirectToMain() {
+	public String redirectToMain(RedirectAttributes redirectAttributes) {
 		return "redirect:/main";
 	}
 	
